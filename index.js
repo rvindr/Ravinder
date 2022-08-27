2d1040225f09d01da6f7038332c47da66c734ed0
Ravinder verma <rvindrvrma@Ravinders-MacBook-Air.local>
2022-08-27 22:26:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const ht =document.getElementById("html");
+const cs =document.getElementById("css");
+const js =document.getElementById("javascript");
+const bs =document.getElementById("bs");
+const tc =document.getElementById("tc");
+const ang =document.getElementById("ang");
+const py=document.getElementById("python");
+
+window.onload =()=>{
+    setTimeout(() => {
+        ht.style.width="90%";
+    cs.style.width="75%";
+    js.style.width="70%";
+    bs.style.width="85%";
+    tc.style.width="80%";
+    ang.style.width="45%";
+    py.style.width="55%";
+    }, 1200);
+}
